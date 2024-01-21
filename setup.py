e410dff8fc25ce838b291af8d51c3853972acf90
Mark <markreed99@gmail.com>
2024-01-21 08:16:16 -0800

    V0.8

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ m1 = Extension(
 
 setup(
   name="mrhttp", 
-  version="0.7",
+  version="0.8",
   license='MIT',
   description='A python web framework written in C',
   long_description=open('README.md').read(),
